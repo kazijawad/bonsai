9c42c7ad9e47f09c839e422870dc0ba22a09f5d3
Kazi Jawad <kazi.a.jawad@gmail.com>
2023-05-10 15:06:44 -0400

    set default mipmap filter to trilinear filter

--- src/base/mipmap.rs
+++ src/base/mipmap.rs
@@ -36,28 +36,12 @@ impl MIPMap {
     }
 
     pub fn filter(&self, st: &mut Point2F, dst0: &mut Vec2F, dst1: &mut Vec2F) -> RGBSpectrum {
-        let width = 2.0
-            * dst0[0]
-                .abs()
-                .max(dst0[1].abs())
-                .max(dst1[0].abs())
-                .max(dst1[1].abs());
-
-        // Compute MIPMap level for width and handle very wide filter.
-        let num_levels = self.levels() as Float;
-
-        let level = num_levels - 1.0 + width.max(1e-8).log2();
-        if level >= num_levels - 1.0 {
-            return self.texel(self.levels() - 1, &Point2I::new(0, 0));
-        }
-
-        let level_i = (level.floor() as usize).max(0);
-        let resolution = self.level_resolution(level_i);
-        let st_i = Point2I::new(
-            (st[0] * resolution[0] as Float - 0.5).round() as i32,
-            (st[1] * resolution[1] as Float - 0.5).round() as i32,
-        );
-        self.texel(level_i, &st_i)
+        let width = dst0[0]
+            .abs()
+            .max(dst0[1].abs())
+            .max(dst1[0].abs())
+            .max(dst1[1].abs());
+        self.trilinear_filter(st, width)
     }
 
     pub fn trilinear_filter(&self, st: &Point2F, width: Float) -> RGBSpectrum {
